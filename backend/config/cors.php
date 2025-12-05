feba4193e8d0ee912d150f178df008e7e50bf31a
ampere07 <164369957+ampere07@users.noreply.github.com>
2025-12-05 10:48:36 +0800

    feat

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,6 +19,7 @@ return [
         'http://localhost:3000',
         'http://localhost:8000',
         'http://127.0.0.1:8000',
+        'http://192.168.100.10:8000',
     ],
 
     'allowed_origins_patterns' => [],
