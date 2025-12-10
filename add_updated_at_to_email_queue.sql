500b30ab203eb833e5cac1e2da4061d9318fcef2
ampere07 <164369957+ampere07@users.noreply.github.com>
2025-12-10 10:35:58 +0800

    feat

--- add_updated_at_to_email_queue.sql
+++
@@ -1,3 +0,0 @@
--- Add updated_at column to email_queue table
-ALTER TABLE `email_queue` 
-ADD COLUMN `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP AFTER `created_at`;
